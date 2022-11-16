579feb939fc17ecfccb242bfe89ccfee2e772ff0
CSIWill <91165874+CSIWill@users.noreply.github.com>
2022-11-16 12:19:34 -0500

    test and index

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>CSI 3660 Project Webpage</title>
+</head>
+<body>
+<h1>Welcome to my CSI 3660 website</h1>
+<p>Description of my project</p>
+<?php
+echo '<img src="https://media.tenor.com/m_WQpKuW5yIAAAAd/evangelion-congratulations.gif"/>';
+?>
+</body>
+</html>
